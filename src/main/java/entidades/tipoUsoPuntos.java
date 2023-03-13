dd9b5f47c99b142bad37d597c7909e97e55db433
matias <fuxesosa@gmail.com>
2023-03-13 19:22:19 -0300

    add entidad tipoUsoPuntos

---
+++ src/main/java/entidades/tipoUsoPuntos.java
@@ -0,0 +1,60 @@
+package entidades;
+
+import jakarta.persistence.*;
+
+@Entity
+public class tipoUsoPuntos {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String descripcion;
+
+    private Long puntosRequeridos;
+
+    public tipoUsoPuntos() {
+    }
+
+    public tipoUsoPuntos(String descripcion, Long puntosRequeridos) {
+        this.descripcion = descripcion;
+        this.puntosRequeridos = puntosRequeridos;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Long getPuntosRequeridos() {
+        return puntosRequeridos;
+    }
+
+    public void setPuntosRequeridos(Long puntosRequeridos) {
+        this.puntosRequeridos = puntosRequeridos;
+    }
+    public String toStringShort() {
+        return "Uso de puntos: \n" +
+                "descripcion: " + descripcion + "\n" +
+                "puntos requeridos: " + puntosRequeridos + "\n";
+    }
+
+    @Override
+    public String toString() {
+        return "tipoUsoPuntos{\n" +
+                "id=" + id +
+                ",\n descripcion='" + descripcion + '\'' +
+                ",\n puntosRequeridos=" + puntosRequeridos +
+                "\n}";
+    }
+}
